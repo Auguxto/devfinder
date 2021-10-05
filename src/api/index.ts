45e36bce5a44bb6c58a9b9d84f387067797bc385
Victor <victoraugusferreira@gmail.com>
2021-10-05 19:29:35 -0300

    Change api token

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.github.com/graphql",
   headers: {
-    Authorization: `Bearer ${process.env.TOKEN}`,
+    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
     "Content-Type": "application/json",
   },
 });
